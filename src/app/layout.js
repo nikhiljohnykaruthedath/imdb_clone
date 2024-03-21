3dc129f8e78bbe95ca0f454d5272ee9a81658754
Nikhil Johny Karuthedath <nikhiljohnykaruthedath@Nikhils-MacBook-Pro.local>
2024-03-21 13:15:04 +0530

    Add darkmode functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import { Inter } from "next/font/google";
+import Providers from "./Providers";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,8 +14,10 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body className={inter.className}>
-				<Header />
-				{children}
+				<Providers default="system" attribute="class">
+					<Header />
+					{children}
+				</Providers>
 			</body>
 		</html>
 	);
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
+import DarkModeSwitch from "./DarkModeSwitch";
 import MenuItem from "./MenuItem";
 
 export default function Header() {
@@ -11,12 +12,15 @@ export default function Header() {
 				<MenuItem title="home" address="/" Icon={AiFillHome} />
 				<MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
 			</div>
-			<Link href={"/"} className="flex gap-1 items-center">
-				<span className="bg-amber-500 text-2xl font-bold py-1 px-2 rounded-lg">
-					IMDb
-				</span>
-				<span className="text-xl sm:inline hidden">Clone</span>
-			</Link>
+			<div className="flex items-center gap-4">
+				<DarkModeSwitch />
+				<Link href={"/"} className="flex gap-1 items-center">
+					<span className="bg-amber-500 text-2xl font-bold py-1 px-2 rounded-lg">
+						IMDb
+					</span>
+					<span className="text-xl sm:inline hidden">Clone</span>
+				</Link>
+			</div>
 		</div>
 	);
 }
